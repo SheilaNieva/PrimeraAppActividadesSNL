cb34dfccf9ce1ad21a4570829bcc71d4ae9b85ce
SheilaNieva <sheilanievalo.2000@gmail.com>
2020-10-14 10:50:50 +0200

    Opción añadir contacto terminada
    actividad añadir contacto terminada

--- app/src/main/java/com/example/primeraappactividadessnl/AnadirContactoActivity.kt
+++ app/src/main/java/com/example/primeraappactividadessnl/AnadirContactoActivity.kt
@@ -1,20 +1,50 @@
 package com.example.primeraappactividadessnl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import kotlinx.android.synthetic.main.activity_anadir_contacto.*
+import kotlinx.android.synthetic.main.activity_mandar_correo.*
 
 class AnadirContactoActivity : AppCompatActivity() {
+
+    //Declaracion de variables
+    private var nombre:String=""
+    private var email:String=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anadir_contacto)
+
+
         val actionbar = supportActionBar
         actionbar!!.title = "Añadir Contacto"
         actionbar.setDisplayHomeAsUpEnabled(true)
         actionbar.setDisplayHomeAsUpEnabled(true)
+
+        //Llamamos al boton añadir contacto, pasandole todos los datos (nombre, email)
+        btnContactoAnadir.setOnClickListener{
+            this.nombre = etContactoNombre.text.toString()
+            this.email = etContactoEmail.text.toString()
+
+            insertContact(nombre,email)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    fun insertContact(name: String, email: String) {
+        val intent = Intent(Intent.ACTION_INSERT).apply {
+            type = ContactsContract.Contacts.CONTENT_TYPE
+            putExtra(ContactsContract.Intents.Insert.NAME, name)
+            putExtra(ContactsContract.Intents.Insert.EMAIL, email)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
